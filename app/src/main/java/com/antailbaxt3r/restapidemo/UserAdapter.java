e291e9fef9715ee35640a83280eaba4cb7973853
antailbaxt3r <arjunbajpaiwork@gmail.com>
2020-04-08 20:16:56 +0530

    UserAdapter Finished

--- app/src/main/java/com/antailbaxt3r/restapidemo/UserAdapter.java
+++ app/src/main/java/com/antailbaxt3r/restapidemo/UserAdapter.java
@@ -1,16 +1,59 @@
 package com.antailbaxt3r.restapidemo;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class UserAdapter extends RecyclerView.Adapter<> {
+import com.antailbaxt3r.restapidemo.models.Result;
+
+import java.util.ArrayList;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder> {
+
+    private ArrayList<Result> userList;
+
+    public UserAdapter(ArrayList<Result> userList) {
+        this.userList = userList;
+    }
+
+    @NonNull
+    @Override
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new UserViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_user, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
+        holder.populate(userList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return userList.size();
+    }
 
     class UserViewHolder extends RecyclerView.ViewHolder{
 
+        private TextView name, gender, phone, email;
         public UserViewHolder(@NonNull View itemView) {
             super(itemView);
+            name = itemView.findViewById(R.id.name);
+            email = itemView.findViewById(R.id.email);
+            phone = itemView.findViewById(R.id.phone);
+            gender = itemView.findViewById(R.id.gender);
+        }
+
+        public void populate(Result result){
+            String nameString = result.getName().getTitle() + " " + result.getName().getFirst() + " " + result.getName().getLast();
+            name.setText(nameString);
+            gender.setText(result.getGender());
+            email.setText(result.getEmail());
+            phone.setText(result.getPhone());
         }
     }
 }
